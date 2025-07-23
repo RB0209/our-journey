e1864dd00690076f270926d4d0eb1f5dfe3e9684
Rishikesh <kesm3052021@gmail.com>
2025-07-23 16:49:14 +0530

    Create page.js

---
+++ page.js
@@ -0,0 +1,10 @@
+import Timeline from "../components/Timeline";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <h1 className="text-4xl font-bold text-center pt-12 mb-8">Our Journey</h1>
+      <Timeline />
+    </main>
+  );
+}
